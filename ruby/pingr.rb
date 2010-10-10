483ea089cbd2df74f14a97d4a1a107179bc78ea0
Tyler Van Hoomissen <linuxsable@gmail.com>
2010-10-10 11:49:50 -0700

    folder changes

--- ruby/pingr.rb
+++ ruby/pingr.rb
@@ -2,26 +2,27 @@
 # Enjoy!
 require 'net/smtp'
 require 'twiliolib'
+require File.dirname(__FILE__) + '/lib/helpers'
 
-HOSTS = ['google.com']
+HOSTS = ['google.com', '192.168.1.1']
 EMAIL = 'linuxsable@gmail.com'
 PHONE_NUMBER = '408'
 PING_COUNT = 4
 
-puts '-- Pinger running --'
+puts '-- Pingr running --'
 start_time = Time.now
 
 HOSTS.each do |host|
-  puts "Checking #{host}"
-  c = `ping -c #{PING_COUNT} #{host} | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }'`
+  puts "Checking #{host}.."
+  c = run_ping(host, PING_COUNT)
   if c.to_i === 0
     message = "Host: #{host} is down (ping failed) at #{Time.now.strftime("%B %d, %Y")}"
     puts message
-    `echo "#{message}" | mail -s "ALERT: Host #{host} is down!" #{EMAIL}`
+    send_email_alert(message, EMAIL, host)
   else
     puts "Host: #{host} is up"
   end
 end
 
 elapsed_time = ("%.2f" % (Time.now - start_time))
-puts "-- Pinger done. Took #{elapsed_time} seconds --"
+puts "-- Pingr done. Took #{elapsed_time} seconds --"
